a8ccb382dcad485ab613426991d2a7743e452781
Shajeeb T Muhammad <shajeeb@outlook.com>
2022-12-28 10:27:27 -0800

    Update KollavarshamTest.ino

--- example/KollavarshamTest.ino
+++ example/KollavarshamTest.ino
@@ -1,1 +1,43 @@
-aa
+
+// Refer https://github.com/shajeebtm/KollaVarsham for details
+
+#include <KollaVarsham.h>
+
+void setup() {
+  Serial.begin(115200);
+  print_kollavarsham();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void print_kollavarsham(){
+  //Serial.println("Starting");
+  
+  KollaVarsham my_kv;
+  
+  int yyyy=2021;
+  int mm=9;
+  int dd=1;
+  Serial.print("Gregorian date    : Year = "); Serial.print (yyyy) ; 
+  Serial.print(" , Month = "); Serial.print (mm) ;
+  Serial.print(" , Day  = "); Serial.println (dd) ;
+
+  int kyyyy = my_kv.getYear();
+  int kmm = my_kv.getMonth();
+  int kdd = my_kv.getDay();
+  
+  
+  my_kv.fromGregorian(yyyy,mm,dd);
+  kyyyy = my_kv.getYear();
+  kmm = my_kv.getMonth();
+  kdd = my_kv.getDay();
+  
+  Serial.print("Kollavarsham date : Year = "); Serial.print (kyyyy) ; 
+  Serial.print(" , Month = "); Serial.print (kmm) ;
+  Serial.print(" , Day = "); Serial.println (kdd) ;
+
+}
+ 
